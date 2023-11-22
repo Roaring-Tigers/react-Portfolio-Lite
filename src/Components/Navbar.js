fb9c60e87f0c1c9dfd08c225372dc7ad70ea12e4
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-22 11:40:23 +0530

    portfolio Lite

---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+// import React from "react"; 
+
+const Navbar = () => {
+
+
+    return(
+        <nav className="navbar">
+               <h1>Abhishek Shankar Choudhary</h1>
+               <div className="menu">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Qualification</a>
+                    <a href="#">Skills</a>
+               </div>
+        </nav>
+    )
+}
+
+export default Navbar;
